42fd7b92f19d26834fe98e80531adbe6d6b3a528
fb0sh <fb0sh@outlook.com>
2024-10-12 15:33:57 +0800

    添加系统托盘

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,11 @@ mod structs;
 mod util;
 mod xzmu;
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
+#[cfg(not(any(target_os = "android", target_os = "ios")))]
 use tauri_plugin_autostart::MacosLauncher; // 仅在非 Android 和 iOS 上导入 MacosLauncher
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -22,10 +27,29 @@ pub fn run() {
 
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
     {
-        app_builder = app_builder.plugin(tauri_plugin_autostart::init(
-            MacosLauncher::LaunchAgent,
-            Some(vec!["--flag1", "--flag2"]), // 向应用传递的参数
-        ));
+        app_builder = app_builder
+            .plugin(tauri_plugin_autostart::init(
+                MacosLauncher::LaunchAgent,
+                Some(vec!["--flag1", "--flag2"]), // 向应用传递的参数
+            ))
+            .setup(|app| {
+                let quit_i = MenuItem::with_id(app, "quit", "关闭程序", true, None::<&str>)?;
+                let menu = Menu::with_items(app, &[&quit_i])?;
+
+                let _tray = TrayIconBuilder::new()
+                    .icon(app.default_window_icon().unwrap().clone())
+                    .menu(&menu)
+                    .menu_on_left_click(false)
+                    .on_menu_event(|app, event| match event.id.as_ref() {
+                        "quit" => {
+                            println!("quit");
+                            app.exit(0);
+                        }
+                        _ => {}
+                    })
+                    .build(app)?;
+                Ok(())
+            });
     }
 
     app_builder
