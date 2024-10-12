b5b57f3d988674f96385939ea77c14d7d11886bc
fb0sh <fb0sh@outlook.com>
2024-10-12 15:11:03 +0800

    no scale

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+window.onload = function () {
+  document.addEventListener("touchstart", function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  });
+  document.addEventListener("gesturestart", function (event) {
+    event.preventDefault();
+  });
+};
 </script>
 
 <template>
